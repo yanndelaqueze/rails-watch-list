1aca90e037f0732531c0729cf5177d6d47a3001d
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-02-24 15:39:55 +0100

    ....

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[index show new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
